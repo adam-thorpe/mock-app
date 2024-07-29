ff344fadc0ea273f9bc873b9f0eabace2d8f85f7
Adam Thorpe <athorpe@caci.co.uk>
2024-07-29 11:50:26 +0100

    Updated code to spring boot app

---
+++ src/test/java/mock/app/ApplicationTest.java
@@ -0,0 +1,13 @@
+package mock.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+	@Test
+	void trueTest() {
+		assertEquals(true, true);
+	}
+}
